c82bf038e2b76e859251013a961304da45a5bb46
EmilyClaudet <emily@kano.me>
2016-05-08 15:40:08 +0100

    added some jumping and music

--- platformer/src/main.lua
+++ platformer/src/main.lua
@@ -1,13 +1,17 @@
 local STI = require "libs.STI"
-local playerX = 100
-local playerY = 580
+local playerX = 0
+local playerY = 550
 local speed = 200
+local playerY_velocity = 0
+local gravity = 500
+local jump_height = 400
+
 
 function love.load()
 	map = STI.new("res/maps/map.lua")
-
 	player = love.graphics.newImage("res/images/jumper.png")
-
+	music = love.audio.newSource("res/audio/cheery.wav")
+	jump_sound = love.audio.newSource("res/audio/jump.wav")
 end
 
 function love.draw()
@@ -15,6 +19,8 @@ function love.draw()
 	map:draw()
 	-- Draw player
 	love.graphics.draw(player, playerX, playerY, 0)
+	-- Play music
+	music:play()
 end
 
 function love.update(dt)
@@ -25,11 +31,30 @@ function love.update(dt)
 		playerX = playerX + (speed * dt)
 	end
 
+	if playerY_velocity ~= 0 then -- We're probably jumping
+    playerY = playerY + (playerY_velocity * dt) -- "dt" means we wont move at different speeds if the game lags
+    playerY_velocity = playerY_velocity - gravity * dt
+ 
+    if playerY < 550 then -- We hit the ground again
+      playerY_velocity = 0
+      playerY = 550
+    end
+  end
+
 	if love.keyboard.isDown('escape') then
 		love.event.quit()
 	end
 end
 
+function love.keypressed(key, scancode, isrepeat)
+  if key == "space" then
+    if playerY_velocity == 0 then -- We're probably on the ground, let's jump
+      playerY_velocity = jump_height
+    end
+    jump_sound:play()
+  end
+end
+
 function love.quit()
-	print('Ending game...')
+	print('Quitting platformer...')
 end
